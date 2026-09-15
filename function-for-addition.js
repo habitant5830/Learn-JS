a54e43dbb414dbb0799a55479e4a9927f37c5b88
habitant5830 <265566328+habitant5830@users.noreply.github.com>
2026-09-15 05:41:48 +0530

    new exercises

---
+++ function-for-addition.js
@@ -0,0 +1,6 @@
+var a = 10;
+var b = 34;
+function add() {
+  console.log(a + b);
+}
+add();
---
+++ function-with-message.js
@@ -0,0 +1,9 @@
+var factor = "mouni";
+var fplayer = "ronaldo";
+var fmovie = "karyasthan";
+function favourite() {
+  console.log("Favourite Actor:", factor);
+  console.log("Favourite Player:", fplayer);
+  console.log("Favourite Movie:", fmovie);
+}
+favourite();
